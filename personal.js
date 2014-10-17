8ccfb8a0497eb4643c373ba364a3132ecfd3e86e
Savannah Henderson <savannahjune@gmail.com>
2014-10-17 12:06:05 -0700

    extra parantheses in personal js

--- personal.js
+++
@@ -1,4 +0,0 @@
-var surpriseHover = document.getElementById("hidden-pop-up");
-	supriseHover.addEventListener("hover", function(e) {
-		supriseHover.alert("This is a black rhino! There are only about 5,000 left in the world.");
-	};
