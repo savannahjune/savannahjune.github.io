9cd797dfaab91e89b3027aeacfc5b05674c31269
Savannah Henderson <savannahjune@gmail.com>
2014-10-17 12:04:49 -0700

    add the file

---
+++ personal.js
@@ -0,0 +1,4 @@
+var surpriseHover = document.getElementById("hidden-pop-up");
+	supriseHover.addEventListener("hover", function(e) {
+		supriseHover.alert("This is a black rhino! There are only about 5,000 left in the world.");
+	};
